7ca8ed80f3811c9d95c0ac06185abd55e23cc5a3
Raul Kolaric <raul.kolaric@gmail.com>
2025-05-03 23:56:04 -0300

    add - supabase

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import platform
 from rich import print
 from rich.console import Console
 from rich.terminal_theme import MONOKAI
-import subprocess
-
-def open_terminal(): 
-    command = f"start cmd.exe /K mode con: cols={30} lines={30}"
-    subprocess.run(command, shell=True)
 
 def clear_terminal():
     if platform.system() == "Windows":
@@ -16,14 +11,13 @@ def clear_terminal():
         os.system("clear")
 
 def main():
-    open_terminal()
-    os.system("mode con: cols=120 lines=10")
     clear_terminal()
     console = Console()
-    console.size = (1000, 1000)
-    
-    # console.print("[bold red]Hello World![/bold red]")
-    print("[italic red]Hello[/italic red] World!",)
+
+    console.print([1, 2, 3])
+    console.print("[blue underline]Looks like a link")
+    console.print(locals())
+    console.print("FOO", style="white on blue")
 
 if __name__ == "__main__":
     main()
